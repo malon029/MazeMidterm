e154d11fbaf632e7be08e6e5e3fcc2de6896be78
Brianna <malon029@cougars.csusm.edu>
2018-10-24 17:21:20 -0700

    First commit

---
+++ MazeLevel1/Assets/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void PlayGame()
+
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void QuitGame()
+    {
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
